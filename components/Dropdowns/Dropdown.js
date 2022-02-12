362caa558728a66c222155f7109671635c680e90
Noah <bitterhippo@gmail.com>
2022-02-12 23:14:11 +0900

    Update Dropdown.js
    additional styling for the box

--- components/Dropdowns/Dropdown.js
+++ components/Dropdowns/Dropdown.js
@@ -4,13 +4,29 @@ import Colors from "../Colors";
 const Dropdown = ({ options }) => {
 
   const [toggleOpen, setToggleOpen] = useState(false);
+  const [hoverdItem, setHoveredItem] = useState('');
+
+  const defaults = ['Lol', 'Does', 'This', 'Work'];
+
+  const openDiv = () => {
+    return <div style={styles.containerOpen}>
+      {
+        defaults.map(current => <div
+        key={current}
+        onMouseEnter={() => setHoveredItem(current)}
+        onMouseLeave={() => setHoveredItem('')}
+        style={{ backgroundColor: hoverdItem === current ? 'red' : Colors.Secondary}}
+        >{current}</div>)
+      }
+    </div>
+  }
 
   return (
     <div
       style={styles.dropdownMainWrapper}
-      onClick={() => setToggleOpen(true)}>
+      onClick={() => setToggleOpen(!toggleOpen)}>
       TEST
-      {toggleOpen && <div style={styles.containerOpen}>OPEN</div>}
+      {toggleOpen && openDiv()}
     </div >
   )
 };
@@ -23,8 +39,12 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     backgroundColor: Colors.Secondary,
-    color: 'white'
-  }
+    color: 'white',
+    width: 200,
+    opacity: 0.6,
+    zIndex: 1,
+    border: '1px solid black'
+  },
 }
 
 export default Dropdown;
