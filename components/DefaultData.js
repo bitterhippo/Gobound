23c92e94591069e839f2999e8eca823748ef6857
Noah <bitterhippo@gmail.com>
2022-02-16 23:38:38 +0900

    created template for default data

---
+++ components/DefaultData.js
@@ -0,0 +1,14 @@
+const defaultData = [{
+  Company: 'Preferred Skills / 歓迎スキル',
+  HP: 'https://www.hopes-ise.co.jp/en/',
+  Career: 'Mid',
+  Position: 'SE',
+  JD: 'https://www.careercross.com/job/detail-1245466?sid=70375510&page=1',
+  Age: '20~28',
+  Income: 'blank',
+  Japanese: 'N1 ~ N2',
+  Condiitons: `ビジネス業務系システムの企画・設計・開発・テスト・運用保守の一貫システムの開発プロジェクトにて、プログラミング業務に携わっていただきます。【必須】・何らかのプログラミング経験(保守も可)1年以上お持ちの方 (Java、C#などのバックエンド技術が望ましい)【尚可】 ・詳細設計、基本設計の経験1年〜3年お持ちの方＊外国籍の場合：日本語レベル：ビジネス会話レベル以上(JLPT N2~N1)`,
+  PreferredSkills: 'none'
+}];
+
+export default defaultData;
--- pages/jobs.js
+++ pages/jobs.js
@@ -1,32 +1,26 @@
 function Jobs() {
 
-  const Testeronies = (name, contract, country) => <tr>
-    <td>{name}</td>
-    <td>{contract}</td>
-    <td>{country}</td>
-  </tr>
-
   const data = [
-    {name: 'lolbert', contract: 'some dumb shit', country: 'amerika'},
-    {name: 'lolbert1', contract: 'some dumb shit', country: 'amerika'},
-    {name: 'lolbert2', contract: 'some dumb shit', country: 'amerika'}
+    { name: 'lolbert', contract: 'some dumb shit', country: 'amerika' },
+    { name: 'lolbert1', contract: 'some dumb shit', country: 'amerika' },
+    { name: 'lolbert2', contract: 'some dumb shit', country: 'amerika' }
   ];
 
   return <div>
     <table>
       <tr>
-        <th>Company</th>
-        <th>Contract</th>
-        <th>Country</th>
+        <th>Company / 企業名</th>
+        <th>HP / 企業URL</th>
+        <th>Career / 新卒/中途</th>
+        <th>Position / ポジション</th>
+        <th>JD / 求人URL</th>
+        <th>Age / 年齢</th>
+        <th>Income / 年収</th>
+        <th>Japanese /日本語</th>
+        <th>Conditions / 必須条件</th>
+        <th>Preferred Skills / 歓迎スキル</th>
       </tr>
       {/* Data to go here */}
-      {
-        data.map(({name, contract, country}) => <tr>
-          <td>{name}</td>
-          <td>{contract}</td>
-          <td>{country}</td>
-        </tr>)
-      }
     </table>
   </div>
 };
