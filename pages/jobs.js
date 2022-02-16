8b66ab5fd426dd80e8149d08b16743f1ad33522f
Noah <bitterhippo@gmail.com>
2022-02-16 23:01:56 +0900

    Update jobs.js

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,6 +1,31 @@
 function Jobs() {
+
+  const Testeronies = (name, contract, country) => <tr>
+    <td>{name}</td>
+    <td>{contract}</td>
+    <td>{country}</td>
+  </tr>
+
+  const data = [
+    {name: 'lolbert', contract: 'some dumb shit', country: 'amerika'},
+    {name: 'lolbert1', contract: 'some dumb shit', country: 'amerika'},
+    {name: 'lolbert2', contract: 'some dumb shit', country: 'amerika'}
+  ];
+
   return <div>
-    Jobs
+    <table>
+      <tr>
+        <th>Company</th>
+        <th>Contract</th>
+        <th>Country</th>
+      </tr>
+      {/* Data to go here */}
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Andes</td>
+        <td>Germany</td>
+      </tr>
+    </table>
   </div>
 };
 
