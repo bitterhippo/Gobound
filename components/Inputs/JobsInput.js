2009f490ff3401198f8bdc0cb6b0e077ec0cdd51
Noah <bitterhippo@gmail.com>
2022-03-01 21:39:27 +0900

    jobsinput.js > refactored defaults
    defaults cleaned up to simplify and shrink size of file. also added some nifty destructuring (for practice)

--- components/Inputs/JobsInput.js
+++ components/Inputs/JobsInput.js
@@ -10,32 +10,35 @@ const JobsInput = () => {
     setSelectedButton(caller);
   };
 
+  //Button Defaults
+
+  const buttonDefaults = [
+    { title: 'Option 1', key: 1, value: 'option1' },
+    { title: 'Option 2', key: 2, value: 'option2' }
+  ];
+
   console.log(selectedButton);
 
   return (
     <div style={styles.jobsInputWrapper}>
       <input placeholder={'Enter your query here'} />
       <div style={styles.jobsInputButtons}>
-        <div>
-          <label>
-          <input 
-          type="radio" 
-          value="buttonOne" 
-          onChange={(e) => handleChange(e)} 
-          checked={selectedButton === 'buttonOne'}/>
-          Option 1
-          </label>
-        </div>
-        <div>
-          <label>
-          <input 
-          type="radio" 
-          value="buttonTwo" 
-          onChange={(e) => handleChange(e)} 
-          checked={selectedButton === 'buttonTwo'}/>
-          Option 2
-          </label>
-        </div>
+        {
+          buttonDefaults.map(({title, key, value}) =>
+            <div
+              key={key}
+            >
+              <label>
+                <input
+                  type="radio"
+                  value={value}
+                  onChange={(e) => handleChange(e)}
+                  checked={selectedButton === value} />
+                {title}
+              </label>
+            </div>
+          )
+        }
       </div>
     </div>
   )
