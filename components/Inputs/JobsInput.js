6cd8853ddeb20b523166a3ed5370b442efed319b
Noah <bitterhippo@gmail.com>
2022-03-01 22:26:51 +0900

    Update JobsInput.js

--- components/Inputs/JobsInput.js
+++ components/Inputs/JobsInput.js
@@ -1,14 +1,22 @@
 import { useState, useEffect } from 'react';
-
+import Colors from '../Colors';
 
 const JobsInput = () => {
 
   const [selectedButton, setSelectedButton] = useState();
+  const [input, setInput] = useState();
+
+  //Event handlers
 
   const handleChange = e => {
     let caller = e.target.value;
     setSelectedButton(caller);
   };
+
+  const handleInputChange = e => {
+    let caller = e.target.value;
+    setInput(caller);
+  }
 
   //Button Defaults
 
@@ -17,14 +25,30 @@ const JobsInput = () => {
     { title: 'Option 2', key: 2, value: 'option2' }
   ];
 
+  //Testing block
+
+  console.log('%c Button Selection', 'color: blue')
   console.log(selectedButton);
+
+  console.log('%c Input Value', 'color: red')
+  console.log(input);
 
   return (
     <div style={styles.jobsInputWrapper}>
-      <input placeholder={'Enter your query here'} />
+      <div style={styles.jobsInputRow}>
+        <input
+          placeholder={'Enter your query here'}
+          onChange={(e) => handleInputChange(e)}
+          style={styles.jobsInput}
+        />
+        <button
+          style={styles.jobsInputButton}
+        >Submit
+        </button>
+      </div>
       <div style={styles.jobsInputButtons}>
         {
-          buttonDefaults.map(({title, key, value}) =>
+          buttonDefaults.map(({ title, key, value }) =>
             <div
               key={key}
             >
@@ -47,11 +71,27 @@ const JobsInput = () => {
 const styles = {
   jobsInputWrapper: {
     height: 200,
-    backgroundColor: 'green'
+    display: 'flex',
+    flexDirection: 'column',
   },
   jobsInputButtons: {
     display: 'flex',
     flexDirection: 'row',
+  },
+  jobsInputRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 25
+  },
+  jobsInput: {
+    width: 150
+  },
+  jobsInputButton: {
+    backgroundColor: Colors.Secondary,
+    color: 'white',
+    fontWeight: 'bold',
+    borderRadius: '5px',
+    border: '1px solid black'
   }
 };
 
